7ff429809a51491e012d13b90c0f721dfe4303d6
UP2021407 <up2021407@myport.ac.uk>
2022-12-08 05:14:29 +0000

    Created Data Model

---
+++ dataModel/dataModel.js
@@ -0,0 +1,37 @@
+// First, import the necessary libraries and dependencies
+const tf = require('@tensorflow/tfjs'); // For machine learning
+const natural = require('natural'); // For natural language processing
+
+// Next, define the model architecture
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+
+// Compile the model with a loss function and an optimizer
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// Train the model on a dataset of past emergency calls
+model.fit(emergencyCallData, emergencyOutcomeData, {epochs: 100});
+
+// Define a function to make predictions on new emergency calls
+function predictEmergencySeverity(callerDescription) {
+  // Use natural language processing to extract relevant information from the caller's description
+  const relevantInfo = extractRelevantInfo(callerDescription);
+
+  // Use the trained model to make a prediction on the likelihood of a severe situation
+  const prediction = model.predict(relevantInfo);
+
+  // Return the prediction to the caller
+  return prediction;
+}
+
+// Define a function to handle incoming emergency calls
+function handleEmergencyCall(callerDescription) {
+  // Use the predictEmergencySeverity function to make a prediction on the likelihood of a severe situation
+  const prediction = predictEmergencySeverity(callerDescription);
+
+  // If the prediction indicates a high likelihood of a severe situation, prioritize the call for immediate dispatch
+  if (prediction >= 0.8) {
+    dispatchEmergencyTeam(callerDescription);
+  }
+}
